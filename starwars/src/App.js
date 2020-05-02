996751c0e833da0eb2d8a72593218236d5e90dbe
Taylor <tpm8472@gmail.com>
2020-05-02 13:24:29 -0600

    question answers

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">STAR WARS CHARACTERS</h1>
       <div className="card">
         {characterData.map(data => {
           return <Character chars={data} />
